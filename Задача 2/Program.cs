7b0f38107b7853bed0ef4232c78a49a3ce0262f3
Maxim <lollollol3328@gmail.com>
2022-11-18 17:36:27 +0300

    Homework

---
+++ Задача 2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Write A: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Write B: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2)
+{
+    Console.WriteLine("A больше B");
+}
+else
+{
+    if (number1 < number2)
+    {
+        Console.WriteLine("B больше A");
+    }
+    else
+    {
+        Console.WriteLine("A Равно B");
+    }
+}
---
+++ Задача4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число :");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число :");
+int number2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число :");
+int number3 = int.Parse(Console.ReadLine()!);
+
+int max = number1;
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+    max = number3;
+}
+
+Console.WriteLine($"Максимальное число = {max}");
